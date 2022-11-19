fdaa32b061b754c0a97324b1f2e02e299312590b
LegoScrap-Development <118598009+LegoScrap-Development@users.noreply.github.com>
2022-11-19 04:50:01 -0500

    Create config.lua

---
+++ client/config.lua
@@ -0,0 +1,34 @@
+Config = {
+    {
+        pos={x=-428.0, y=1132.0, z=325.8}, -- Position of the teleporter
+        tpto={x=1968.0, y=3711.0, z=32.3}, -- Where you want the teleporter to take you
+        placename = "Sandy Shores", -- The destination name
+        marker = 33, -- Marker to show above the circle on the ground (https://docs.fivem.net/docs/game-references/markers/)
+        scale = 4.0, -- Scale of the teleporter
+        rgba = {255, 120, 120,155} -- Color of the teleporter
+    },
+    {
+        pos={x=-435.0, y=1132.0, z=325.8},
+        tpto={x=153.5, y=6561.3, z=32.0},
+        placename = "Paleto Bay",
+        marker = 33,
+        scale = 4.0,
+        rgba = {120, 255, 120,155}
+    },
+    {
+        pos={x=-418.5, y=1129.5, z=325.8},
+        tpto={x=-1030.2, y=-2882.2, z=14.0},
+        placename = "LSIA",
+        marker = 33,
+        scale = 4.0,
+        rgba = {120, 120, 255,155}
+    },
+    {
+        pos={x=-413.0, y=1126.0, z=325.8},
+        tpto={x=195.2, y=-933.9, z=30.7},
+        placename = "Los Santos",
+        marker = 33,
+        scale = 4.0,
+        rgba = {255, 255, 120,155}
+    },
+}
